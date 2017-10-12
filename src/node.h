5532a3fd7914585d11434e0fdc5016e4af1e9107
scomup <scomup@gmail.com>
2017-10-12 19:28:45 +0900

    first

---
+++ src/node.h
@@ -0,0 +1,19 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include<stdint.h>
+
+namespace Scheduler
+{
+
+struct Node
+{
+    int16_t iD;
+    int16_t core_num;
+    int16_t time;
+    std::vector<int16_t> sub_nodes;
+};
+
+}
+
+#endif
